564111634333fe751ee7f82c3fd66f7d4969cc90
ssmith777 <shawn.t.smith.777@gmail.com>
2021-08-08 12:19:42 -0500

    :sparkles: Section 2.2 completed :tada:

---
+++ README.md
@@ -0,0 +1,1 @@
+# ssmith777-scala-rock-the-jvm
---
+++ src/lectures/part1Basics/ValuesVariablesTypes.scala
@@ -0,0 +1,42 @@
+package lectures.part1Basics
+
+object ValuesVariablesTypes extends App {
+  val x: Int = 42
+
+  //val x = 42
+  // Types are optional
+  println(x)
+
+  //x=2
+  // VALS ARE IMMUTABLE
+
+  // String
+  val aString: String = "hello"
+
+  // Boolean
+  val aBoolean: Boolean = false
+
+  // Char
+  val aChar: Char ='a'
+
+  // Int
+  val anInt: Int = x
+
+  // Short
+  val aShort: Short = 22222
+
+  // Long
+  val aLong: Long = 1217777777777L
+
+  // Float
+  val aFloat: Float =2.0f
+
+  // Double
+  val aDouble: Double = 3.14
+
+  //Variables
+  var aVarable: Int = 4
+
+  aVarable = 5 // Note: This is known as a side effect
+
+}
