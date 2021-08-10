a42c3aedd262e2b47bef91d291334d342b77f19e
ssmith777 <shawn.t.smith.777@gmail.com>
2021-08-10 16:23:56 -0500

    :memo: Made some changes to the Readme, finshed section 2.4

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# scala-rock-the-jvm
+# scala-rock-the-jvm 
+
+### Scala: Identifiers
+_Identifiers_ are used to identify class names, method named, variable names, and object names. There are six kinds of identifiers in Scala: 
+- GRG (class)
+- a (variable name)
+- main (method name) 
+- Main (object name) 
+- ob (object name)
+- and args (variable name)
 
 ## Functional Programming Rules
 _*DO NOT USE LOOPS*_ 
 
-Note: Scala does have loops if there is no other way around solving your paradigm 
+Note: Scala does have loops, however only use if there is no other way around solving your paradigm 
 
 #### Example Loop in Scala
 ```
@@ -26,3 +35,8 @@ Note: Scala does have loops if there is no other way around solving your paradig
 ### Functional Programming Notes:
 - In Imperative languages like (Java, c++) you are executing a set of instructions (Do this, then that, then this). 
 - In functional programming you execute expressions, Think "It gives me the value of something"
+
+
+### Scala Links
+- Note: Amanda Fawcett done a great job on this article
+[Scala 101: a beginner's guide to the scalable language](https://dev.to/educative/scala-101-a-beginner-s-guide-to-the-scalable-language-1lgk)
--- src/lectures/part1Basics/Functions.scala
+++ src/lectures/part1Basics/Functions.scala
@@ -13,10 +13,48 @@ object Functions extends App {
   println(aParameterLessFunctions())
 
   // This is an example of a Recursive Function 🤪
+  // Note: That all recursive functions require a return type
   def aRepeatedFunction(aString: String, n: Int): String = {
     if (n == 1) aString
     else aString + " : " + aRepeatedFunction(aString, n-1)
   }
   println(aRepeatedFunction("Charvel Guitars Rock 🎸 ", 9))
 
+  def aFunctionWithSideEffects(aString: String): Unit = println(aString)
+  aFunctionWithSideEffects("Gibson SG")
+
+  def aBigFunction(n: Int): Int = {
+    def aSmallerFunction(a: Int, b: Int): Int = a + b
+    aSmallerFunction(n, n-1)
+  }
+  println(aBigFunction(12))
+
+  def greetingForKids(name: String, age: Int): String =
+   s"Hi, my name is ${name} and I am ${age} years old"
+
+  println(greetingForKids("Shawn", 45))
+
+  // Another recursive function example
+  def factorial(n: Int): Int =
+    if (n <= 0) 1
+    else n * factorial(n - 1)
+
+  println(factorial(5))
+
+  // Fibonacci Sequence
+  def fibonacci(n: Int): Int =
+    if (n <= 2) 1
+    else fibonacci(n-1) + fibonacci(n-2)
+
+  // 1 1 2 3 5 8 13 21
+  println(fibonacci(8))
+
+  def isPrime(n: Int): Boolean = {
+    def isPrimeUntil(t: Int): Boolean =
+      if (t <= 1) true
+      else n % t != 0 && isPrimeUntil(t-1)
+
+    isPrimeUntil(n/2)
+  }
+  println(isPrime(5))
 }
